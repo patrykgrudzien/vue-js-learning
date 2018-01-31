df44cde932029c0ed68fc7ea747890e9cb7077b4
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-01-31 17:09:54 +0100

    Lesson 11 and beginning of 12.

---
+++ lesson-11/app.js
@@ -0,0 +1,19 @@
+new Vue({
+	// this Vue instance is gonna control #vue-app element
+	el: '#vue-app',
+
+	data: {
+		error: false,
+		success: false,
+		show: false
+	},
+
+	methods: {
+
+	},
+
+	computed: {
+
+	}
+
+});
