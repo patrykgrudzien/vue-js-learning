1934edc13c5bcb809618e88de2caffde3b7af2e0
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-02-16 10:29:21 +0100

    Added vue-loading-spinner

--- using-vue-cli/src/lesson-43-44/components/showBlogs.vue
+++ using-vue-cli/src/lesson-43-44/components/showBlogs.vue
@@ -3,6 +3,8 @@
     <h1>All Blog Articles</h1>
     <input type="text" v-model="search" placeholder="Search blogs by title">
 
+    <circle-spinner v-if="blogsLoading" style="margin: 0 auto"/>
+
     <div v-for="blog in filteredBlogs" class="single-blog">
       <router-link class="title" v-bind:to="`/blog/${blog.id}`"><h2>{{ blog.title }}</h2></router-link>
       <p>{{ blog.author }}</p>
@@ -16,29 +18,37 @@
 
 <script>
   import searchMixin from '../mixins/searchMixin';
+  import CircleSpinner from 'vue-loading-spinner/src/components/Circle8'
 
   export default {
+    components: {
+      'circle-spinner': CircleSpinner
+    },
     data() {
       return {
         blogs: [],
-        search: ''
+        search: '',
+        blogsLoading: true
       }
     },
     methods: {},
     created() {
-      this.axios.get('https://vue-js-simple-blog-project.firebaseio.com/posts.json').then((response) => {
+      this.axios.get('https://vue-js-simple-blog-project.firebaseio.com/posts.json')
+          .then((response) => {
+            this.blogsLoading = false;
+            let blogsArray = [];
 
-        let blogsArray = [];
-        for (let key in response.data) {
-          // here we want to add additional property "id" to each blog we're currently on in this for loop
-          response.data[key].id = key;
-          blogsArray.push(response.data[key]);
-        }
-        this.blogs = blogsArray;
+            for (let key in response.data) {
+            // here we want to add additional property "id" to each blog we're currently on in this for loop
+            response.data[key].id = key;
+            blogsArray.push(response.data[key]);
+            }
 
-      }).catch((error) => {
-        console.log(error);
-      })
+            this.blogs = blogsArray;
+
+          }).catch((error) => {
+            console.log(error);
+          })
     },
     computed: {},
     mixins: [searchMixin]
--- using-vue-cli/src/lesson-43-44/components/singleBlog.vue
+++ using-vue-cli/src/lesson-43-44/components/singleBlog.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="single-blog">
+    <circle-spinner v-if="blogsLoading" style="margin: 0 auto"/>
     <h1>{{ blog.title }}</h1>
     <article>{{ blog.blogContent }}</article>
   </div>
 </template>
 
 <script>
+  import CircleSpinner from 'vue-loading-spinner/src/components/Circle8'
+
   export default {
+    components: {
+      'circle-spinner': CircleSpinner
+    },
     data() {
       return {
         // we can grab here ROUTE PARAMETER (request param)
         // "id" is out param we specified in "allRoutes.js" (path: '/blog/:id')
         id: this.$route.params.id,
-        blog: {}
+        blog: {},
+        blogsLoading: true
       }
     },
     created() {
       // LOOK CAREFULLY at this URL
       this.axios.get(`https://vue-js-simple-blog-project.firebaseio.com/posts/${this.id}.json`)
           .then((response) => {
+            this.blogsLoading = false;
             console.log(response);
             this.blog = response.data;
           });
